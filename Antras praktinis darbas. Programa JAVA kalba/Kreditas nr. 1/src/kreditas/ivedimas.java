7d74c39c57dc32f29cdd0dc3817a82384de07bca
Heartless27 <zbignevas114@gmail.com>
2015-12-16 01:46:14 +0200

    Perkuriamas projektas nes nera atidaromas su NetBeans programa. Pridedami pradiniai failai

--- Antras praktinis darbas. Programa JAVA kalba/Kreditas nr. 1/src/kreditas/ivedimas.java
+++ Antras praktinis darbas. Programa JAVA kalba/Kreditas nr. 1/src/kreditas/ivedimas.java
@@ -3,61 +3,56 @@ package kreditas;
 import java.util.Scanner;
 
 public class ivedimas {
-	public static void main(String []args){
-		Scanner scan = new Scanner(System.in);
-		vkreditas VartClass = new vkreditas();
-		bkreditas BustClass = new bkreditas();
-		double Pajamos, PajamosB, skolos, Ksuma; // ivedimo
-		int variantas, Svaiku, pam, laik;// ivedimo
-		
-		int sumavaik = 100;
-		System.out.println(" ");
-		System.out.println("Pasirinkite kredità: "
-				+ "Bûsto Kreditas - 1" + " "
-				+ "Vartojamasis Kreditas - 2");
-		variantas = scan.nextInt();
-		
-		
-		if(variantas <= 2){ 
-			if (variantas==2){
-				System.out.println("Áveskite vaikø skaièiu nuo 0 iki 10 :");
-				Svaiku = scan.nextInt();
-				System.out.println("Áveskite pajamas atskaièiavus mokesèius:");
-				Pajamos = scan.nextDouble();
-				System.out.println("Áveskite ásiskolinimø skaièiø:");
-				skolos = scan.nextDouble();
-				System.out.println("Áveskite kredito laikotarpi iki 5 metø:");
-				laik = scan.nextInt();
-				System.out.println("Áveskite kredito suma nuo 300 Eu iki 26000 Eu: ");
-				Ksuma = scan.nextDouble();
-				PajamosB=Pajamos-skolos-(Svaiku*sumavaik);
-				VartClass.setDuom(PajamosB, Ksuma, laik, laik);
-				VartClass.kreditas();
-				}
-			else {	
-				System.out.println("Áveskite vaikø skaiciu nuo 0 iki 10 :");
-				Svaiku = scan.nextInt();
-				System.out.println("Áveskite pajamas atskaièiavus mokesèius:");
-				Pajamos = scan.nextDouble();
-				System.out.println("Áveskite ásiskolinimø skaièiø: ");
-				skolos = scan.nextDouble();
-				System.out.println("Áveskite pastato amþiø: ");
-				pam = scan.nextInt();
-				System.out.println("Áveskite kredito laikotarpá: ");
-				laik = scan.nextInt();
-				System.out.println("áveskite kredito sumà: ");
-				Ksuma = scan.nextDouble();
-				PajamosB=Pajamos-skolos-(Svaiku*sumavaik);
-				BustClass.setDuom(PajamosB, Ksuma, laik, skolos, pam);
-				BustClass.kreditas();
-			}
-		}
-		else System.out.println("Neteisingas parinktas parametras!");
-	
-	}
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        vkreditas VartClass = new vkreditas();
+        bkreditas BustClass = new bkreditas();
+        double Pajamos, PajamosB, skolos, Ksuma; // ivedimo
+        int variantas, Svaiku, pam, laik;// ivedimo
+
+        int sumavaik = 100;
+        System.out.println(" ");
+        System.out.println("Pasirinkite kreditï¿½: "
+                + "Bï¿½sto Kreditas - 1" + " "
+                + "Vartojamasis Kreditas - 2");
+        variantas = scan.nextInt();
+
+        if (variantas <= 2) {
+            if (variantas == 2) {
+                System.out.println("ï¿½veskite vaikï¿½ skaiï¿½iu nuo 0 iki 10 :");
+                Svaiku = scan.nextInt();
+                System.out.println("ï¿½veskite pajamas atskaiï¿½iavus mokesï¿½ius:");
+                Pajamos = scan.nextDouble();
+                System.out.println("ï¿½veskite ï¿½siskolinimï¿½ skaiï¿½iï¿½:");
+                skolos = scan.nextDouble();
+                System.out.println("ï¿½veskite kredito laikotarpi iki 5 metï¿½:");
+                laik = scan.nextInt();
+                System.out.println("ï¿½veskite kredito suma nuo 300 Eu iki 26000 Eu: ");
+                Ksuma = scan.nextDouble();
+                PajamosB = Pajamos - skolos - (Svaiku * sumavaik);
+                VartClass.setDuom(PajamosB, Ksuma, laik, laik);
+                VartClass.kreditas();
+            } else {
+                System.out.println("ï¿½veskite vaikï¿½ skaiciu nuo 0 iki 10 :");
+                Svaiku = scan.nextInt();
+                System.out.println("ï¿½veskite pajamas atskaiï¿½iavus mokesï¿½ius:");
+                Pajamos = scan.nextDouble();
+                System.out.println("ï¿½veskite ï¿½siskolinimï¿½ skaiï¿½iï¿½: ");
+                skolos = scan.nextDouble();
+                System.out.println("ï¿½veskite pastato amï¿½iï¿½: ");
+                pam = scan.nextInt();
+                System.out.println("ï¿½veskite kredito laikotarpï¿½: ");
+                laik = scan.nextInt();
+                System.out.println("ï¿½veskite kredito sumï¿½: ");
+                Ksuma = scan.nextDouble();
+                PajamosB = Pajamos - skolos - (Svaiku * sumavaik);
+                BustClass.setDuom(PajamosB, Ksuma, laik, skolos, pam);
+                BustClass.kreditas();
+            }
+        } else {
+            System.out.println("Neteisingas parinktas parametras!");
+        }
+
+    }
 }
-		
-
-	
-
-
