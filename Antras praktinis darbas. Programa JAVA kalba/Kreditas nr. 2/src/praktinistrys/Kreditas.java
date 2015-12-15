9839158a11e496e39f00d4a3c92d67bae37c30de
Heartless27 <zbignevas114@gmail.com>
2015-12-15 23:49:22 +0200

    Sukuriu nauja klase, panaudodamas Replace method with Method object ir isvengiu kodo dubliavimo

---
+++ Antras praktinis darbas. Programa JAVA kalba/Kreditas nr. 2/src/praktinistrys/Kreditas.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktinistrys;
+
+/**
+ *
+ * @author E J A
+ */
+public class Kreditas {
+
+    private double paskolosSuma;
+    private double sutartiesSudarymoMokestisAtsakymas;
+    private double atsakymas1 = 0;
+    private double atsakymas2 = 0;
+
+    private double paskolosSuma2;
+    private double laikotarpis;
+    private double bendrosGrazintinosSumosApskaiciavimasAtsakymas;
+
+    private double kreditoMenesineImokaApskaiciavimas;
+
+    private double realiosMenesinesPajamos;
+    private double seimosPajamosAtskaiciusMokescius;
+    private double seimosFinansiniaiIsipareigojimai;
+    private double vaikuSkaiciusSeimoje;
+
+    public double getPaskolosSuma() {
+        return paskolosSuma;
+    }
+
+    public void setPaskolosSuma(double paskolosSuma) {
+        this.paskolosSuma = paskolosSuma;
+    }
+
+    public double getSutartiesSudarymoMokestisAtsakymas() {
+        return sutartiesSudarymoMokestisAtsakymas;
+    }
+
+    public void setSutartiesSudarymoMokestisAtsakymas(double sutartiesSudarymoMokestisAtsakymas) {
+        this.sutartiesSudarymoMokestisAtsakymas = sutartiesSudarymoMokestisAtsakymas;
+    }
+
+    public double getAtsakymas1() {
+        return atsakymas1;
+    }
+
+    public void setAtsakymas1(double atsakymas1) {
+        this.atsakymas1 = atsakymas1;
+    }
+
+    public double getAtsakymas2() {
+        return atsakymas2;
+    }
+
+    public void setAtsakymas2(double atsakymas2) {
+        this.atsakymas2 = atsakymas2;
+    }
+
+    public double getPaskolosSuma2() {
+        return paskolosSuma2;
+    }
+
+    public void setPaskolosSuma2(double paskolosSuma2) {
+        this.paskolosSuma2 = paskolosSuma2;
+    }
+
+    public double getLaikotarpis() {
+        return laikotarpis;
+    }
+
+    public void setLaikotarpis(double laikotarpis) {
+        this.laikotarpis = laikotarpis;
+    }
+
+    public double getBendrosGrazintinosSumosApskaiciavimasAtsakymas() {
+        return bendrosGrazintinosSumosApskaiciavimasAtsakymas;
+    }
+
+    public void setBendrosGrazintinosSumosApskaiciavimasAtsakymas(double bendrosGrazintinosSumosApskaiciavimasAtsakymas) {
+        this.bendrosGrazintinosSumosApskaiciavimasAtsakymas = bendrosGrazintinosSumosApskaiciavimasAtsakymas;
+    }
+
+    public double getKreditoMenesineImokaApskaiciavimas() {
+        return kreditoMenesineImokaApskaiciavimas;
+    }
+
+    public void setKreditoMenesineImokaApskaiciavimas(double kreditoMenesineImokaApskaiciavimas) {
+        this.kreditoMenesineImokaApskaiciavimas = kreditoMenesineImokaApskaiciavimas;
+    }
+
+    public double getRealiosMenesinesPajamos() {
+        return realiosMenesinesPajamos;
+    }
+
+    public void setRealiosMenesinesPajamos(double realiosMenesinesPajamos) {
+        this.realiosMenesinesPajamos = realiosMenesinesPajamos;
+    }
+
+    public double getSeimosPajamosAtskaiciusMokescius() {
+        return seimosPajamosAtskaiciusMokescius;
+    }
+
+    public void setSeimosPajamosAtskaiciusMokescius(double seimosPajamosAtskaiciusMokescius) {
+        this.seimosPajamosAtskaiciusMokescius = seimosPajamosAtskaiciusMokescius;
+    }
+
+    public double getSeimosFinansiniaiIsipareigojimai() {
+        return seimosFinansiniaiIsipareigojimai;
+    }
+
+    public void setSeimosFinansiniaiIsipareigojimai(double seimosFinansiniaiIsipareigojimai) {
+        this.seimosFinansiniaiIsipareigojimai = seimosFinansiniaiIsipareigojimai;
+    }
+
+    public double getVaikuSkaiciusSeimoje() {
+        return vaikuSkaiciusSeimoje;
+    }
+
+    public void setVaikuSkaiciusSeimoje(double vaikuSkaiciusSeimoje) {
+        this.vaikuSkaiciusSeimoje = vaikuSkaiciusSeimoje;
+    }
+    
+
+ 
+
+}
